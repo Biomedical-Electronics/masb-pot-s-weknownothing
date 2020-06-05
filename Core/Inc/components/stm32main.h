8d1fa5555ff30cbea9e35f3a68b4de0700720418
Omilen Pangue <omilenpangue@gmail.com>
2020-06-05 18:37:35 +0200

    creation of setup and loop functions

---
+++ Core/Inc/components/stm32main.h
@@ -0,0 +1,19 @@
+/*
+ * stm32main.h
+ *
+ *  Created on: 5 jun. 2020
+ *      Author: omile
+ */
+
+#ifndef INC_COMPONENTS_STM32MAIN_H_
+#define INC_COMPONENTS_STM32MAIN_H_
+
+#include "stm32f4xx_hal.h"
+#include "cobs.h"
+
+//function prototypes
+void setup(void);
+void loop(void);
+
+
+#endif /* INC_COMPONENTS_STM32MAIN_H_ */
---
+++ Core/Src/components/stm32main.c
@@ -0,0 +1,19 @@
+#include "components/stm32main.h"
+
+// executed function before while loop (just once)
+void setup(void){
+
+}
+
+//executed function in the while loop
+void loop(void){
+
+}
+/*
+ * stm32main.c
+ *
+ *  Created on: 5 jun. 2020
+ *      Author: omile
+ */
+
+
